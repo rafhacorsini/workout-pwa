98aa3f460ea44c44a86b6e9782a9a93fcf3e497f
rafhacorsini <rafhaelcorsini@gmail.com>
2025-12-05 23:08:02 -0300

    fix: include env.js for vercel deployment

---
+++ src/js/env.js
@@ -0,0 +1,6 @@
+
+export const ENV = {
+    // Public keys for PWA (Safe to expose if RLS is enabled)
+    SUPABASE_URL: 'https://hnuvpzgkprjznyvavtpx.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhudXZwemdrcHJqem55dmF2dHB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzQ3NjAsImV4cCI6MjA4MDU1MDc2MH0.vGOEyoVFGCBAuxkb7JhiodieK4tPw6qsw4iBS1W4arg'
+};
